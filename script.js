b90f70886daff7529d7d0d4e2db18f3b5d0b1a50
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-28 10:59:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Check for an empty string or no space
+  if (str === '' || !str.includes(' ')) {
+    return str;
+  }
+
+  // Extract the first word (characters up to the first space)
+  const firstSpaceIndex = str.indexOf(' ');
+  const firstWord = str.substring(0, firstSpaceIndex);
+
+  return firstWord;
 }
 
 // Do not change the code below
